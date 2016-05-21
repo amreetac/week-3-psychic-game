bc4b6016a4486b753a1cdb6e3adc34fde0b090e1
Amreeta Choudhury <amreeta89@yahoo.com>
2016-05-20 23:33:35 -0400

    Moved JS code to JS file

---
+++ assets/javascript/game.js
@@ -0,0 +1,58 @@
+
+    /*Step 1: How to activate the key commands and then
+    define the variables.
+
+    Step 2: For userGuess, we take in the input from event
+    For computerGuess, it is randomized from
+    computerChoices*/
+
+    /* Set the computer choices. */
+    var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+    /*Declare the tallies to zero */
+    var wins = 0;
+    var losses = 0;
+    var guesses = 9;
+    var arr = [];
+
+
+/*Records the keypress and then sets it to the user press. */
+/* Sets the computer to the random numbers., */
+    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+    document.onkeyup = function(event) {
+
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+
+                arr.push(userGuess);
+                guesses--;
+                if (userGuess == computerGuess) {
+                    wins++; guesses = 9;
+                    computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+                    arr = [];
+                }
+                if (guesses <= 0) {
+                    losses++; guesses = 9;
+                    computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+                    arr = [];
+                }
+
+
+
+            //Taking the tallies and displaying them in HTML
+
+            var html = "<p> Guess what letter I am thinking of </p>" +
+            "<p> wins: " + wins + "</p>" +
+            "<p> losses: " + losses + "</p>" +
+            "<p> guesses left: " + guesses + "</p>" +
+            "<p> Your guesses so far: " + arr + "</p>";
+
+            // Placing the html in to the game id.
+
+
+
+
+    document.querySelector('#game').innerHTML = html;
+
+    }
+
