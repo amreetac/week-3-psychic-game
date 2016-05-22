c84c1b114d23640f2b8fc9e2fe6f61bb9f5125a8
Amreeta Choudhury <amreeta89@yahoo.com>
2016-05-21 21:32:46 -0400

    Made some indenting changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,23 +9,24 @@
     /* Set the computer choices. */
     var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-    /*Declare the tallies to zero */
+    /* Initialize */
     var wins = 0;
     var losses = 0;
     var guesses = 9;
     var arr = [];
 
-
-/*Records the keypress and then sets it to the user press. */
 /* Sets the computer to the random numbers., */
+/*Records the keypress and then sets it to the user press. */
+
     var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
     document.onkeyup = function(event) {
 
     var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
-                arr.push(userGuess);
-                guesses--;
+        arr.push(userGuess);
+        guesses--;
+
                 if (userGuess == computerGuess) {
                     wins++; guesses = 9;
                     computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
@@ -41,16 +42,13 @@
 
             //Taking the tallies and displaying them in HTML
 
-            var html = "<p> Guess what letter I am thinking of </p>" +
-            "<p> wins: " + wins + "</p>" +
-            "<p> losses: " + losses + "</p>" +
-            "<p> guesses left: " + guesses + "</p>" +
-            "<p> Your guesses so far: " + arr + "</p>";
+    var html = "<p> Guess what letter I am thinking of </p>" +
+                "<p> wins: " + wins + "</p>" +
+                "<p> losses: " + losses + "</p>" +
+                "<p> guesses left: " + guesses + "</p>" +
+                "<p> Your guesses so far: " + arr + "</p>";
 
             // Placing the html in to the game id.
-
-
-
 
     document.querySelector('#game').innerHTML = html;
 
